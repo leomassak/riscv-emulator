4b2a8111ba7cd77b822c7e5830d8d9108123144f
Leonardo Massak <leoo.lcm23@hotmail.com>
2022-04-19 21:02:41 -0300

    instructions decode finished

--- riscv_emulator.c
+++ riscv_emulator.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 
-#define XLEN 32
+#define ARCHLEN 32
 
 #define RAM_SIZE 0b0010000000000000000
 
@@ -17,35 +17,11 @@ uint8_t ram[RAM_SIZE];
 uint32_t pc;
 uint32_t next_pc;
 uint32_t inst;
-uint32_t reg[32];
+uint32_t reg[ARCHLEN];
 
-uint32_t ram_start;
+uint32_t ram_start = 0;
 
-uint64_t inst_count = 0;
-
-
-int target_read_u8(uint8_t *pval, uint32_t addr)
-{
-    addr -= ram_start;
-    if (addr > RAM_SIZE) {
-        *pval = 0;
-        printf("illegal read 8, PC: 0x%08x, address: 0x%08x\n", pc, addr + ram_start);
-        return 1;
-    } else {
-        uint8_t* p = ram + addr;
-        *pval = p[0];
-    }
-    return 0;
-}
-
-uint32_t get_inst(uint32_t pc)
-{
-    uint32_t ptr = pc - ram_start;
-    if (ptr > RAM_SIZE) return 1;
-    uint8_t* p = ram + ptr;
-    return p[0] | (p[1] << 8) | (p[2] << 16) | (p[3] << 24);
-}
-
+unsigned char buffer[ARCHLEN];
 
 void execute_instruction()
 {
@@ -77,7 +53,7 @@ void execute_instruction()
             break;
         case 1: 
             //sll
-            val = (int32_t)(val << (val2 & (XLEN - 1)));
+            val = (int32_t)(val << (val2 & (ARCHLEN - 1)));
             break;
         case 2: 
             //slt
@@ -93,11 +69,11 @@ void execute_instruction()
             break;
         case 5: 
             //srl
-            val = (int32_t)((uint32_t)val >> (val2 & (XLEN - 1)));
+            val = (int32_t)((uint32_t)val >> (val2 & (ARCHLEN - 1)));
             break;
         case 5 | 8:
             //sra
-            val = (int32_t)val >> (val2 & (XLEN - 1));
+            val = (int32_t)val >> (val2 & (ARCHLEN - 1));
             break;
         case 6: 
             //or
@@ -129,11 +105,7 @@ void execute_instruction()
 
     case 0b1101111: 
         //jal
-        imm = ((inst >> (31 - 20)) & (1 << 20)) |
-              ((inst >> (21 - 1)) & 0b11111111110) |
-              ((inst >> (20 - 11)) & (1 << 11)) |
-              (inst & 0xff000);
-        imm = (imm << 11) >> 11;
+        imm = ((inst & 0b11111111000000000000) << 11);
         if (rd != 0)
             reg[rd] = pc + 4;
         next_pc = (int32_t)(pc + imm);
@@ -143,7 +115,7 @@ void execute_instruction()
         //jalr
         imm = (int32_t)inst >> 20;
         val = pc + 4;
-        next_pc = (int32_t)(reg[rs1] + imm) & ~1;
+        next_pc = (int32_t)(reg[rs1] + imm);
         if (rd != 0)
             reg[rd] = val;
         break;
@@ -180,160 +152,138 @@ void execute_instruction()
         }
         break;
 
-    // case 0b0000011: /* LOAD */
+    case 0b0000011: /* LOAD */
 
-    //     funct3 = (inst >> 12) & 7;
-    //     imm = (int32_t)inst >> 20;
-    //     addr = reg[rs1] + imm;
-    //     switch(funct3) {
+        funct3 = (inst >> 12) & 7;
+        imm = (int32_t)inst >> 20;
+        addr = reg[rs1] + imm;
+        switch(funct3) {
 
-    //     case 0: /* lb */
-    //     {
-    //         uint8_t rval;
-    //         if (target_read_u8(&rval, addr)) {
-    //             printf("Endereco de memoria nao suportado");
-    //             return;
-    //         }
-    //         val = (int8_t)rval;
-    //     }
-    //     break;
+        case 0: /* lb */
+        {
+            uint8_t rval;
+    
+            uint8_t* p = ram + addr;
+            rval = p[0];
+            val = (int8_t)rval;
+        }
+        break;
 
-    //     case 1: /* lh */
-    //     {
-    //         uint16_t rval;
-    //         if (target_read_u16(&rval, addr)) {
-    //             raise_exception(pending_exception, pending_tval);
-    //             return;
-    //         }
-    //         val = (int16_t)rval;
-    //     }
-    //     break;
+        case 1: /* lh */
+        {
+            uint16_t rval;
+    
+            uint16_t* p = ram + addr;
+            rval = p[0];
+            val = (int16_t)rval;
+        }
+        break;
 
-    //     case 2: /* lw */
-    //     {
-    //         uint32_t rval;
-    //         if (target_read_u32(&rval, addr)) {
-    //             raise_exception(pending_exception, pending_tval);
-    //             return;
-    //         }
-    //         val = (int32_t)rval;
-    //     }
-    //     break;
+        case 2: /* lw */
+        {
+            uint32_t rval;
 
-    //     case 4: /* lbu */
-    //     {
-    //         uint8_t rval;
-    //         if (target_read_u8(&rval, addr)) {
-    //             raise_exception(pending_exception, pending_tval);
-    //             return;
-    //         }
-    //         val = rval;
-    //     }
-    //     break;
+            uint32_t* p = ram + addr;
+            rval = p[0];
+            val = (int32_t)rval;
+        }
+        break;
 
-    //     case 5: /* lhu */
-    //     {
-    //         uint16_t rval;
-    //         if (target_read_u16(&rval, addr)) {
-    //             raise_exception(pending_exception, pending_tval);
-    //             return;
-    //         }
-    //         val = rval;
-    //     }
-    //     break;
+        case 4: /* lbu */
+        {
+             uint8_t rval;
+    
+            uint8_t* p = ram + addr;
+            rval = p[0];
+            val = rval;
+        }
+        break;
 
-    //     default:
-    //         raise_exception(CAUSE_ILLEGAL_INSTRUCTION, inst);
-    //         return;
-    //     }
-    //     if (rd != 0)
-    //         reg[rd] = val;
-    //     break;
+        case 5: /* lhu */
+        {
+            uint16_t rval;
+    
+            uint16_t* p = ram + addr;
+            rval = p[0];
+            val = rval;
+        }
+        break;
 
-    // case 0b0100011: /* STORE */
+        default:
+            print("Instrucao de Load nao identificada.");
+            return;
+        }
+        if (rd != 0)
+            reg[rd] = val;
+        break;
 
-    //     funct3 = (inst >> 12) & 7;
-    //     imm = rd | ((inst >> (25 - 5)) & 0xfe0);
-    //     imm = (imm << 20) >> 20;
-    //     addr = reg[rs1] + imm;
-    //     val = reg[rs2];
-    //     switch(funct3) {
+    case 0b0100011: /* STORE */
 
-    //     case 0: /* sb */
-    //         if (target_write_u8(addr, val)) {
-    //             raise_exception(pending_exception, pending_tval);
-    //             return;
-    //         }
-    //         break;
+        uint32_t h = inst & 0b111111100000000000000000000000;
+        uint32_t l = inst & 0b000000000000000000000000011111;
+                                                
+        funct3 = (inst >> 12) & 7;
+        imm = (h >> 18) + l;
+        addr = reg[rs1] + imm;
+        val = reg[rs2];
+        switch(funct3) {
 
-    //     case 1: /* sh */
-    //         if (target_write_u16(addr, val)) {
-    //             raise_exception(pending_exception, pending_tval);
-    //             return;
-    //         }
-    //         break;
+        case 0: /* sb */
+            uint8_t* p = ram + addr;
+            p[0] = val & 0b11111111;
+            break;
 
-    //     case 2: /* sw */
-    //         if (target_write_u32(addr, val)) {
-    //             raise_exception(pending_exception, pending_tval);
-    //             return;
-    //         }
-    //         break;
+        case 1: /* sh */
+            uint8_t* p = ram + addr;
+            p[0] = val & 0b11111111;
+            p[1] = (val >> 8) & 0b11111111;
+            break;
 
-    //     default:
-    //         raise_exception(CAUSE_ILLEGAL_INSTRUCTION, inst);
-    //         return;
-    //     }
-    //     break;
+        case 2: /* sw */
+            uint8_t* p = ram + addr;
+            p[0] = val & 0b11111111;
+            p[1] = (val >> 8) & 0b11111111;
+            p[2] = (val >> 16) & 0b11111111;
+            p[3] = (val >> 24) & 0b11111111;
+            break;
 
-    // case 0b0010011: /* OP-IMM */
+        default:
+            printf("Instrucao de store nao identificada.");
+            return;
+        }
+        break;
 
-    //     funct3 = (inst >> 12) & 7;
-    //     imm = (int32_t)inst >> 20;
-    //     switch(funct3) {
-    //     case 0: /* addi */
-    //         val = (int32_t)(reg[rs1] + imm);
-    //         break;
-    //     case 1: /* slli */
-    //         if ((imm & ~(XLEN - 1)) != 0) {
-    //             raise_exception(CAUSE_ILLEGAL_INSTRUCTION, inst);
-    //             return;
-    //         }
-    //         val = (int32_t)(reg[rs1] << (imm & (XLEN - 1)));
-    //         break;
-    //     case 2: /* slti */
-    //         val = (int32_t)reg[rs1] < (int32_t)imm;
-    //         break;
-    //     case 3: /* sltiu */
-    //         val = reg[rs1] < (uint32_t)imm;
-    //         break;
-    //     case 4: /* xori */
-    //         val = reg[rs1] ^ imm;
-    //         break;
-    //     case 5: /* srli/srai */
-    //         if ((imm & ~((XLEN - 1) | 0x400)) != 0) {
-    //             raise_exception(CAUSE_ILLEGAL_INSTRUCTION, inst);
-    //             return;
-    //         }
-    //         if (imm & 0x400)
-    //         {
-    //             val = (int32_t)reg[rs1] >> (imm & (XLEN - 1));
-    //         }
-    //         else
-    //         {
-    //             val = (int32_t)((uint32_t)reg[rs1] >> (imm & (XLEN - 1)));
-    //         }
-    //         break;
-    //     case 6: /* ori */
-    //         val = reg[rs1] | imm;
-    //         break;
-    //     case 7: /* andi */
-    //         val = reg[rs1] & imm;
-    //         break;
-    //     }
-    //     if (rd != 0)
-    //         reg[rd] = val;
-    //     break;
+    case 0b0010011: /* OP-IMM */
+
+        funct3 = (inst >> 12) & 7;
+        imm = (int32_t)inst >> 20;
+        switch(funct3) {
+        case 0: /* addi */
+            val = (int32_t)(reg[rs1] + imm);
+            break;
+        case 1: /* slli */
+            break;
+        case 2: /* slti */
+            val = (int32_t)reg[rs1] < (int32_t)imm;
+            break;
+        case 3: /* sltiu */
+            val = reg[rs1] < (uint32_t)imm;
+            break;
+        case 4: /* xori */
+            val = reg[rs1] ^ imm;
+            break;
+        case 5: /* ori */
+            break;
+        case 6: /* ori */
+            val = reg[rs1] | imm;
+            break;
+        case 7: /* andi */
+            val = reg[rs1] & imm;
+            break;
+        }
+        if (rd != 0)
+            reg[rd] = val;
+        break;
 
     default:
         printf("Instrução não reconhecida");
@@ -347,8 +297,7 @@ void riscv_decoder()
     while (1) {
         next_pc = pc + 4;
 
-        inst = get_inst(pc);
-        inst_count++;
+        inst = buffer;
         execute_instruction();
 
         pc = next_pc;
@@ -357,18 +306,27 @@ void riscv_decoder()
 
 int main(int argc, char** argv)
 {
+    FILE *ptr;
 
-    /* automatic STDOUT flushing, no fflush needed */
-    setvbuf(stdout, NULL, _IONBF, 0);
+    ptr = fopen("test.bin","rb");
 
-    /* run program in emulator */
+    
+    if (!ptr) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    } 
+
+    fread(buffer,sizeof(buffer),1,ptr);
+
     pc = ram_start;
     riscv_decoder();
 
+    printf("Registradores:\n");
+    for(int i = 0; i < ARCHLEN; i++) {
+        printf("R[%i]: %i\n", i, reg[i]);
+    }
+
     printf("\n");
-    printf(">>> Instruction count: %llu\n",(long long unsigned)inst_count);
-    // printf(">>> Jumps: %llu (%2.2lf%%) - %llu forwards, %llu backwards\n",(long long unsigned)jump_counter,jump_counter*100.0/insn_counter,(long long unsigned)forward_counter,(long long unsigned)backward_counter);
-    // printf(">>> Branching T=%llu (%2.2lf%%) F=%llu (%2.2lf%%)\n",(long long unsigned)true_counter,true_counter*100.0/(true_counter+false_counter),(long long unsigned)false_counter,false_counter*100.0/(true_counter+false_counter));
-    printf("\n");
+    fclose(ptr);
     return 0;
 }
